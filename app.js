2b4fc8b919b8073f49bff9d9449f713e1b545a98
Angekarara <mbabange2020@gmail.com>
2024-09-20 17:16:09 +0200

    crude exercise

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("connected to database"))
+
+const itemsSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+})
+
+const Item = mongoose.model("Item", itemsSchema)
+app.use(express.json())
+
+app.post("/items", async (req, res) => {
+    try {
+        const items = await new Item(req.body).save()
+        res.status(200).send(items)
+    } catch (error) {
+    }
+})
+
+app.get("/items", async (req, res) => {
+    try {
+        const items = await Item.find()
+        res.status(200).send(items)
+    } catch (error) {
+        res.status(400).json({error: "not found"})
+    }
+})
+
+app.get("/items/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const item = await Item.findById(id)
+        res.status(200).send(item)
+    } catch (error) {
+        res.status(400).send("bad request")
+    }
+})
+
+app.delete("/items/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const item = await Item.findByIdAndDelete(id)
+        res.status(200).send(item)
+    } catch (error) {
+        res.status(400).send("bad request")
+    }
+})
+
+app.listen(5000)
